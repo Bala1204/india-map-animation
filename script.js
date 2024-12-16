8665ceadcc30cb93d617264f8ee023eb2362784b
Bala1204 <balasathwik.n@gmail.com>
2024-12-16 12:30:02 +0530

    Add files via upload
    git add .
    git commit -m "Added website files"
    git push origin main

---
+++ script.js
@@ -0,0 +1,4 @@
+// Redirect to main interface after animation
+setTimeout(() => {
+  window.location.href = "main.html"; // Replace with the URL or page for the main interface
+}, 4000); // Adjust timing to match animation duration
